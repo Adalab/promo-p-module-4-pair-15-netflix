e4bb0a939b36560390c1a30ae71bbc9ce85e99aa
SandraRial <sandrarialperez@gmail.com>
2022-05-19 17:52:39 +0200

    users database

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const cors = require('cors');
 // const data = require('./data/movies.json');
-const users = require('./data/users.json');
-const Database = require("better-sqlite3");
+//const users = require('./data/users.json');
+const Database = require('better-sqlite3');
 
 // create and config server
 const server = express();
 server.use(cors());
 server.use(express.json());
-server.set("view engine", "ejs");
+server.set('view engine', 'ejs');
 
 // init express aplication
 const serverPort = 4000;
@@ -16,10 +16,11 @@ server.listen(serverPort, () => {
   console.log(`Server listening at http://localhost:${serverPort}`);
 });
 
-const db = Database("./src/db/movies.db", { verbose: console.log });
+const db = Database('./src/db/movies.db', { verbose: console.log });
+const dbUsers = Database('./src/db/users.db', { verbose: console.log });
 
 // Recuperar todas las peliculas del catálogo de Netflix
-server.get("/movies", (req, res) => {
+server.get('/movies', (req, res) => {
   const query = db.prepare(`SELECT  * FROM movies ORDER BY title `);
   //Ejecuto la sentencia SQL
   const movieList = query.all();
@@ -29,13 +30,23 @@ server.get("/movies", (req, res) => {
     movies: movieList,
   };
 
+  // if (req.query.gender === '') {
+  //   const query = db.prepare(`SELECT * FROM movies`);
+  //   dataMovies = query.all();
+  // } else {
+  //   const query = db.prepare(`SELECT * FROM movies WHERE gender = ?`);
+  //   genderList = query.all(dataMovies.movies.gender);
+  //   //return genderList;
+  // }
+  // console.log(genderList);
+
   const genderFilterParam = req.query.gender;
   let response = {};
   if (genderFilterParam === undefined) {
     res.json({ success: false });
   } else {
     const filterGenderMovies = dataMovies.movies.filter((movie) => {
-      if (genderFilterParam === "") {
+      if (genderFilterParam === '') {
         return true;
       }
       return movie.gender === genderFilterParam;
@@ -44,39 +55,48 @@ server.get("/movies", (req, res) => {
   }
 });
 
-server.post("/login", (req, res) => {
-  const userFind = users.find(
-    (user) =>
-      user.email === req.body.email && user.password === req.body.password
+server.post('/login', (req, res) => {
+  //Tomar los datos de la base de datos de usuarios en dbUsers
+  const query = dbUsers.prepare(
+    `SELECT  * FROM users WHERE email = ? AND password = ? `
   );
+  //Ejecuto la sentencia SQL
+  const userFind = query.get(req.body.email, req.body.password);
+  // Condicional para cuando se hace login, encuentre la usuaria en la base de datos
   if (userFind !== undefined) {
     return res.json({ success: true, userId: userFind.id });
   } else {
     return res.json({
       success: false,
-      errorMessage: "Usuaria/o no encontrada/o",
+      errorMessage: 'Usuaria/o no encontrada/o',
     });
   }
 });
 
 // 4.4.
 // 1. Consigue el id de la película que se va a renderizar
-server.get("/movie/:movieId", (req, res) => {
-  // console.log(req.params.movieId);
-  const foundMovie = data.movies.find(
-    (movie) => movie.id === req.params.movieId
+server.get('/movie/:movieId', (req, res) => {
+  const query = db.prepare(
+    `SELECT  * FROM movies WHERE id=${req.params.movieId} `
   );
+  //Ejecuto la sentencia SQL
+  const movieList = query.get();
+
+  // console.log(req.params.movieId);
+  // const foundMovie = data.movies.find(
+  //   (movie) => movie.id === req.params.movieId
+  // );
   // console.log(foundMovie);
-  res.render("movie", foundMovie);
+  res.render('movie', movieList);
 });
 
-const staticServerPathWeb = "./src/public-react"; // En esta carpeta ponemos los ficheros estáticos
+const staticServerPathWeb = './src/public-react'; // En esta carpeta ponemos los ficheros estáticos
 server.use(express.static(staticServerPathWeb));
 
-const staticServerImages = "./src/public-movies-images"; // En esta carpeta ponemos los ficheros estáticos
+const staticServerImages = './src/public-movies-images'; // En esta carpeta ponemos los ficheros estáticos
 server.use(express.static(staticServerImages));
 
-const staticServerCss = "./src/public-css"; // En esta carpeta ponemos los ficheros estáticos
+const staticServerCss = './src/public-css'; // En esta carpeta ponemos los ficheros estáticos
 server.use(express.static(staticServerCss));
 
 //configura la base de datos en Node JS, donde tomamos los datos de movies.db
